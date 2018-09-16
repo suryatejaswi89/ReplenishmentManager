48a87c0be77bbcbcd30770a3482c36f91f01b54e
Teja <suryatejaswi89@gmail.com>
2018-09-16 19:48:36 -0400

    CodeCleanup : moved the business logic to service classes

--- src/main/java/com/replenishmentmanager/controller/TaskController.java
+++ src/main/java/com/replenishmentmanager/controller/TaskController.java
@@ -1,12 +1,7 @@
 package com.replenishmentmanager.controller;
 
-import java.time.LocalDate;
-import java.time.Period;
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -20,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.replenishmentmanager.commons.StatusEnum;
 import com.replenishmentmanager.model.Task;
 import com.replenishmentmanager.model.TaskRepository;
 import com.replenishmentmanager.model.User;
@@ -43,18 +37,14 @@ public class TaskController {
 	@RequestMapping(value = "/task", method = RequestMethod.POST)
 	@ResponseBody
 	public ResponseEntity<Task> createTask(@RequestBody Task task) {
-
-		Task persistedTask = null;
 		try{
-			task.setStatus(StatusEnum.CREATED.toString());
-			task.setWeightage(task.getPriority()*1000/task.getEstimate());
-			persistedTask = taskService.saveTask(task);
-			logger.info("Task with id {} got created successfully", persistedTask.getTaskID());
-			logger.debug("Persisted task with id {} and with details {}", persistedTask.getTaskID(), persistedTask.toString());
-			return new ResponseEntity<Task>(persistedTask, HttpStatus.OK);
+			Task newTask = taskService.createTask(task);
+			logger.info("Task with id {} got created successfully", newTask.getTaskID());
+			logger.debug("Persisted task with id {} and with details {}", newTask.getTaskID(), newTask.toString());
+			return new ResponseEntity<Task>(newTask, HttpStatus.OK);
 		}catch(Exception e){
 			logger.error("Task creation failed with the exception: {}", e.getMessage());
-			return new ResponseEntity<Task>(persistedTask,HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<Task>(task,HttpStatus.BAD_REQUEST);
 		}
 	}
 	
@@ -63,16 +53,13 @@ public class TaskController {
 	public ResponseEntity<List<Task>> getTasksbystatus(@PathVariable(value = "status") String status){
 		List<Task> tasks = null;
 		try{
-		tasks = taskRepo.findAllBystatus(status);
+		tasks = taskService.getTaskbystatus(status);
 		logger.info("Tasks with given status have been found succesfully", tasks);
 		logger.debug("tasks with given status {}",tasks.toString());
 		return new ResponseEntity<List<Task>>(tasks, HttpStatus.OK);
-		//logger.info(arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9);
 		}
 		catch(Exception e){
-			
-			logger.error("fething the task with given task has failed", e.getMessage());
-			
+			logger.error("fetching the task with given task has failed", e.getMessage());
 		}
 		return new ResponseEntity<List<Task>>(tasks, HttpStatus.BAD_REQUEST);
 	}
@@ -80,26 +67,17 @@ public class TaskController {
 	@RequestMapping(value = "/pendingtasks/", method = RequestMethod.GET)
 	@ResponseBody
 	public ResponseEntity<List<Task>> pendingTasks(){
-		
-		List<Task> pendingTasks = new ArrayList<Task>();
-		
+		List<Task> tasks = null;
 		try{
-		pendingTasks.addAll(taskRepo.findAllBystatus(StatusEnum.CREATED.toString()));
-		pendingTasks.addAll(taskRepo.findAllBystatus(StatusEnum.STARTED.toString()));
-	
-		List<Task> sortedTasks = pendingTasks.stream()
-				.sorted(Comparator.comparingDouble(Task::getWeightage).reversed())
-				.collect(Collectors.toList());
-		logger.info("Pending tasks have been listed successfully",sortedTasks);
-		logger.debug("Pending tasks have been listed successfully",sortedTasks.toString());
-		return new ResponseEntity<List<Task>>(sortedTasks, HttpStatus.OK);
+		tasks = taskService.getPendingTasks();
+		logger.info("Pending tasks have been returned",tasks);
+		logger.debug("Pending tasks have been returned",tasks.toString());
+		return new ResponseEntity<List<Task>>(tasks, HttpStatus.OK); 
 		}
 		catch(Exception e){
-			logger.error("feching the pending tasks has failed", e.getMessage());	
+			logger.error("Failed to get pending tasks");
 		}
-		
-		return new ResponseEntity<List<Task>>(pendingTasks, HttpStatus.BAD_REQUEST);
-		
+		return new ResponseEntity<List<Task>>(tasks, HttpStatus.BAD_REQUEST); 
 	}
 	
 	@RequestMapping(value="/tasks/{id}/", method = RequestMethod.GET)
@@ -107,7 +85,7 @@ public class TaskController {
 	public ResponseEntity<Optional<Task>> getTasksbyID(@PathVariable(value = "id") String id){
 		Optional<Task> task = null;
 		try{
-		task = taskRepo.findById(id);
+		task = taskService.getTaskbyID(id);
 		logger.info("Task with given id has been found", task);
 		logger.debug("Task with given id has been found",task.toString());
 		return new ResponseEntity<Optional<Task>>(task,HttpStatus.OK);
@@ -124,7 +102,7 @@ public class TaskController {
 	public ResponseEntity<List<Task>> getAllTasks(){
 		List<Task> tasks = null;
 		try{
-		 tasks = taskRepo.findAll();
+		 tasks = taskService.getAllTasks();
 		 logger.info("All tasks have been listed", tasks);
 		logger.debug("All tasks have been listed",tasks.toString());
 		 return new ResponseEntity<List<Task>>(tasks,HttpStatus.OK);
@@ -142,11 +120,9 @@ public class TaskController {
 	public ResponseEntity<List<Task>> getAllTaskscreatedByUser(@PathVariable(value = "name") String name){
 		List<Task> tasks = null;
 		try{
-		User user = userRepo.findByfName(name);
-		String id = user.getUserId();
-		tasks = taskRepo.findBytaskOwnerID(id);
-		logger.info("All tasks created by user with given id have been listed", id,tasks);
-		logger.debug("All tasks have been listed",id, tasks.toString());
+			tasks = taskService.getTasksbyCreator(name);
+		logger.info("All tasks created by user with given id have been listed", tasks);
+		logger.debug("All tasks have been listed", tasks.toString());
 		return new ResponseEntity<List<Task>>(tasks,HttpStatus.OK);
 		}
 		catch(Exception e){
@@ -160,11 +136,9 @@ public class TaskController {
 	public ResponseEntity<List<Task>> getTasksAssigned(@PathVariable(value = "name") String name){
 		List<Task> tasks = null;
 		try{
-		User user = userRepo.findByfName(name);
-		String id = user.getUserId();
-		tasks = taskRepo.findByassigneeID(id);
-		logger.info("All tasks assigned to user with given id have been listed", id,tasks);
-		logger.debug("All tasks assigned to the have been listed",id, tasks.toString());
+		tasks = taskService.getTasksAssigned(name);
+		logger.info("All tasks assigned to user with given id have been listed",tasks);
+		logger.debug("All tasks assigned to the have been listed", tasks.toString());
 		return new ResponseEntity<List<Task>>(tasks,HttpStatus.OK);
 		}
 		catch(Exception e){
@@ -175,57 +149,16 @@ public class TaskController {
 	
 	@RequestMapping(value = "/updateStatus/{id}/{status}/", method = RequestMethod.PUT)
 	@ResponseBody
-	public ResponseEntity<Task> updateStatus(@PathVariable(value = "id") String id, @PathVariable(value = "status") String status) throws CloneNotSupportedException {
+	public ResponseEntity<Task> updateStatus(@PathVariable(value = "id") String id,
+			@PathVariable(value = "status") String status) throws CloneNotSupportedException {
 		Task task = null;
-		try{
-		Optional<Task> taskWrapper = taskRepo.findById(id);
-
-		if (taskWrapper.isPresent()) {
-			task = taskWrapper.get();
-			logger.info("Task has been found using id", task);
-			task.setStatus(status.toUpperCase());
-			logger.debug("Status for the task has been updated",task.toString());
-			if (status.equalsIgnoreCase(StatusEnum.STARTED.toString())) {
-				LocalDate date = LocalDate.now();
-				task.setDateStarted(date);
-				logger.info("Time stamp when the task is moved to STARTED status has been captured", task.getDateStarted() );;
-				Period period = Period.between(task.getDateCreated(), date);
-				task.setTimeinCreatedstatus(period.getDays());
-				logger.info("Tracked time the task has spent in CREATED status", task.getTimeinCreatedstatus());
-			} else if (status.equalsIgnoreCase(StatusEnum.COMPLETED.toString())) {
-				LocalDate date = LocalDate.now();
-				task.setDateCompleted(date);
-				logger.info("Time stamp when the task is moved to COMPLETED status has been captured", task.getDateCompleted());
-				Period period = Period.between(task.getDateStarted(), date);
-				task.setTimeInDateStartedstatus(period.getDays());
-				logger.info("Tracked time the task has spent in CREATED status", task.getTimeInDateStartedstatus());
-				if(task.isRecurringTask()){
-					//calculate startdatefor the next recurring task
-					logger.info("Task has been identified as a recurring task", task.isRecurringTask());
-					LocalDate currentTaskCreationDate = task.getDateCreated();
-					LocalDate newStartDate = currentTaskCreationDate.plusDays(task.getFrequency());
-					logger.info("Setting the dateCreated for the new task");
-					Task nextRecurringTask = (Task) task.clone();
-					logger.info("new task has been cloned from the old task");
-					nextRecurringTask.setDateCreated(newStartDate);
-					nextRecurringTask.setDateStarted(null);
-					nextRecurringTask.setDateCompleted(null);
-					nextRecurringTask.setStatus(StatusEnum.CREATED.toString());
-					nextRecurringTask.setTaskID(null); // mongo will create and assign new id 
-					taskRepo.save(nextRecurringTask); //this can cause an exception
-					logger.info("Saved the newly created recurring task to DB",nextRecurringTask.toString());
-				}
-			}
-			taskRepo.save(task); //this can cause an excetion
-		} else {
-			// log error that task is null
+		try {
+			task = taskService.updateStatus(id, status);
+			return new ResponseEntity<Task>(task, HttpStatus.OK);
+		} catch (Exception e) {
+			logger.error("Update status failed with error: {}", e.getMessage());
 		}
-		return new ResponseEntity<Task>(task,HttpStatus.OK);
-		}catch(Exception e){
-			
-		}
-
-		return new ResponseEntity<Task>(task,HttpStatus.BAD_REQUEST);
+		return new ResponseEntity<Task>(task, HttpStatus.BAD_REQUEST);
 	}
 	
 	@RequestMapping(value = "/deletetask/{id}/" , method = RequestMethod.DELETE)
--- src/main/java/com/replenishmentmanager/service/TaskService.java
+++ src/main/java/com/replenishmentmanager/service/TaskService.java
@@ -1,11 +1,19 @@
 package com.replenishmentmanager.service;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.replenishmentmanager.commons.StatusEnum;
 import com.replenishmentmanager.model.Task;
 import com.replenishmentmanager.model.TaskRepository;
 import com.replenishmentmanager.model.User;
@@ -20,9 +28,12 @@ public class TaskService {
 	@Autowired
 	private UserRepository userRepo;
 	
-	public Task saveTask(Task task){
-		taskRepo.save(task);
-		return task;
+	final static Logger logger = LogManager.getLogger();
+	
+	public Task createTask(Task task){
+		task.setStatus(StatusEnum.CREATED.toString());
+		task.setWeightage(task.getPriority()*1000/task.getEstimate());
+		return taskRepo.save(task);
 	}
 	
 	public List<Task> getTaskbystatus(String status){
@@ -54,15 +65,79 @@ public class TaskService {
 		return tasks;
 	}
 	
-	public Task updateStatus(String id, String status){
+	public List<Task> getPendingTasks(){
+		List<Task> pendingTasks = new ArrayList<Task>();
+		pendingTasks.addAll(taskRepo.findAllBystatus(StatusEnum.CREATED.toString()));
+		pendingTasks.addAll(taskRepo.findAllBystatus(StatusEnum.STARTED.toString()));
+	
+		List<Task> sortedTasks = pendingTasks.stream()
+				.sorted(Comparator.comparingDouble(Task::getWeightage).reversed())
+				.collect(Collectors.toList());
+		logger.info("Pending tasks have been listed successfully",sortedTasks);
+		logger.debug("Pending tasks have been listed successfully",sortedTasks.toString());
+		return sortedTasks;
+	}
+	
+	//Method to update the status of the task by the given id.
+	public Task updateStatus(String id, String status) throws Exception{
 		Task task = null;
-		Optional<Task> taskwrapper = taskRepo.findById(id);
-		task = taskwrapper.get();
-		task.setStatus(status);
-		taskRepo.save(task);
+		Optional<Task> taskWrapper = taskRepo.findById(id);
+		if (taskWrapper.isPresent()) {
+			task = taskWrapper.get();
+			logger.info("Task has been found using id", task);
+			task.setStatus(status.toUpperCase());
+			logger.debug("Status for the task has been updated",task.toString());
+			if (status.equalsIgnoreCase(StatusEnum.STARTED.toString())) {
+				LocalDate date = LocalDate.now();
+				task.setDateStarted(date);
+				logger.info("Time stamp when the task is moved to STARTED status has been captured", task.getDateStarted() );;
+				Period period = Period.between(task.getDateCreated(), date);
+				task.setTimeinCreatedstatus(period.getDays());
+				logger.info("Tracked time the task has spent in CREATED status", task.getTimeinCreatedstatus());
+			} else if (status.equalsIgnoreCase(StatusEnum.COMPLETED.toString())) {
+				LocalDate date = LocalDate.now();
+				task.setDateCompleted(date);
+				logger.info("Time stamp when the task is moved to COMPLETED status has been captured", task.getDateCompleted());
+				Period period = Period.between(task.getDateStarted(), date);
+				task.setTimeInDateStartedstatus(period.getDays());
+				logger.info("Tracked time the task has spent in CREATED status", task.getTimeInDateStartedstatus());
+				if(task.isRecurringTask()){
+					createRecurringTask(task);
+				}
+			}
+			task = taskRepo.save(task); //this can cause an exception
+		} else {
+			//Throw an exception to let controller know that finding task db query failed
+			throw new Exception("Task Not found");
+		}
 		return task;
 	}
 	
+		//method to create a recurring task.
+		// handling exceptions for recurring task creation in service layer because the response of the updatestatus is not effected
+		// by the outcome of the creating recurring task.
+	public void createRecurringTask(Task task) {
+		try {
+			LocalDate currentTaskCreationDate = task.getDateCreated();
+			LocalDate newStartDate = currentTaskCreationDate.plusDays(task.getFrequency());
+			logger.info("Setting the dateCreated for the new task");
+			Task nextRecurringTask = (Task) task.clone(); //might throw CloneNotSupportedException
+			logger.info("new task has been cloned from the old task");
+			nextRecurringTask.setDateCreated(newStartDate);
+			nextRecurringTask.setDateStarted(null);
+			nextRecurringTask.setDateCompleted(null);
+			nextRecurringTask.setStatus(StatusEnum.CREATED.toString());
+			nextRecurringTask.setTaskID(null); // mongo will create and assign
+												// new id
+			taskRepo.save(nextRecurringTask); // might throw an exception here
+			logger.info("Created a recurring task successfully: {}", nextRecurringTask.getTaskID());
+			logger.debug("Created a recurring task successfully with the following details: {}", nextRecurringTask.toString());
+		} catch (CloneNotSupportedException e) {
+			logger.error("Create recurrring task failed with error: {}", e.getMessage());
+		} catch (Exception e) {
+			logger.error("Failed to create a recurring task", task);
+		}
+	}
 	
 	
 	
