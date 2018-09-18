79ee33288eb55555c656b6030ec55572cd55908a
Teja <suryatejaswi89@gmail.com>
2018-09-18 02:59:33 -0400

    UI Layer: Integrated update status rest API

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
 
+      function passTaskId(currentElement){
+          console.log(currentElement);
+        var currentTaskid =  currentElement.data('id');
+        $("#update_status_modal #ut_task_id").val(currentTaskid);
+      }
+      
     //onload get available tasks and list 
     function getTasks(){
         var settings = {
@@ -14,14 +20,12 @@ $(document).ready(function() {
           }
           
           $.ajax(settings).done(function (response) {
-            console.log(response);
-
             //iterate and append tasks to list
             response.forEach(task => {
                 $("#task_list ul").append('<li class="list-group-item d-flex justify-content-between align-items-center">'+
                 '<span>'+task.description+'</span>'+
                 '<span class="badge badge-primary badge-pill">'+task.status+'</span>'+
-                '<button class="badge badge-secondary" id="update_status_btn" data-toggle="modal" data-target="#update_status_modal">Update Status</button>'+
+                '<button class="badge badge-secondary" data-id='+task.taskID +' id="update_status_btn" data-toggle="modal" data-target="#update_status_modal">Update Status</button>'+
               '</li>');
             });
           });     
@@ -56,8 +60,46 @@ $(document).ready(function() {
 
        $.ajax(settings).done(function (response) {
              console.log(response);
+             window.location.replace("http://localhost:8080")
          });
 
       });
+
+    function updateStatus() {
+        var taskId = $("#ut_task_id").val();
+        var newStatus = $("#ut_status").val();
+        var url = "http://localhost:8080/updateStatus/" + taskId + "/" + newStatus + "/";
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": url,
+            "method": "PUT",
+            "headers": {
+                "Content-Type": "application/json",
+                "Cache-Control": "no-cache"
+            }
+        }
+
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+            window.location.replace("http://localhost:8080")
+        });
+    }
+
+    //logic to pass values to modal
+
+    $('body').on('click', '#update_status_btn', function (e) {
+        // convert target to jquery object
+        var $target = $(e.target);
+        // modal targeted by the button
+        var modalSelector = $target.data('target');
+        var dataValue = $target.data("id");
+        $("#ut_task_id").val(dataValue || '');
+    });
+
+    $("#update_status_submit_btn").click(function(){
+        updateStatus();
+    });
+		
 });
   
