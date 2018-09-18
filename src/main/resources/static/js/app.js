85134ad03af7ab4f1afc01e88c12c5c6cb1fca7b
Teja <suryatejaswi89@gmail.com>
2018-09-18 01:56:33 -0400

    UI Layer: Integrated List all task rest API

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,5 +1,32 @@
 $(document).ready(function() {
-      
+
+    //onload get available tasks and list 
+    function getTasks(){
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "http://localhost:8080/tasks/",
+            "method": "GET",
+            "headers": {
+              "Content-Type": "application/json",
+              "Cache-Control": "no-cache"
+            }
+          }
+          
+          $.ajax(settings).done(function (response) {
+            console.log(response);
+
+            //iterate and append tasks to list
+            response.forEach(task => {
+                $("#task_list ul").append('<li class="list-group-item d-flex justify-content-between align-items-center">'+
+                '<span>'+task.description+'</span>'+
+                '<span class="badge badge-primary badge-pill">'+task.status+'</span>'+
+                '<button class="badge badge-secondary" id="update_status_btn" data-toggle="modal" data-target="#update_status_modal">Update Status</button>'+
+              '</li>');
+            });
+          });     
+    }
+      getTasks();
       $("#create_task_submit_btn").click(function(){
             var task = {};
             task.description = $("#ct_description").val();
